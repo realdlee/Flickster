acd17252c5eb1ab11cd948e413304302fe07300f
David Lee <ambitiouspear@gmail.com>
2016-07-21 00:25:58 -0700

    Complete videowalkthrough
    * load movies in custom array adapter that displays title, overview, and poster

--- app/src/main/java/com/lee/flickster/MovieActivity.java
+++ app/src/main/java/com/lee/flickster/MovieActivity.java
@@ -3,7 +3,9 @@ package com.lee.flickster;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ListView;
 
+import com.lee.flickster.adapters.MovieArrayAdapter;
 import com.lee.flickster.models.Movie;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -18,11 +20,20 @@ import cz.msebera.android.httpclient.Header;
 
 public class MovieActivity extends AppCompatActivity {
     ArrayList<Movie> movies;
+    MovieArrayAdapter movieAdapter;
+    ListView lvItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        lvItems = (ListView) findViewById(R.id.lvMovies);
+        movies = new ArrayList<>();
+        movieAdapter = new MovieArrayAdapter(this, movies);
+        lvItems.setAdapter(movieAdapter);
+
         String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(url, new JsonHttpResponseHandler(){
@@ -32,7 +43,8 @@ public class MovieActivity extends AppCompatActivity {
 
                 try {
                     movieJsonResults = response.getJSONArray("results");
-                    movies = Movie.fromJSONArray(movieJsonResults);
+                    movies.addAll(Movie.fromJSONArray(movieJsonResults));
+                    movieAdapter.notifyDataSetChanged();
                     Log.d("DEBUG", movies.toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
